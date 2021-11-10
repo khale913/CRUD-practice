8a3c77cb144c8605823b2deed97e88ff70f67e9d
Kelton Hale <keltonhale33@gmail.com>
2021-11-10 09:37:29 -0700

    add todo functionality, and partial render functionality

--- index.js
+++ index.js
@@ -14,10 +14,30 @@ form.addEventListener("submit", (e) => {
 });
 
 function addTodo() {
+  // input value is new TODO
   const newTodo = input.value;
-
+  // check for todo inputs, if none return
   if (!newTodo) return;
 
-  todoList.push(newTodo);
+  // push the newtodo input into the todoList array
+  todoList.push({
+    text: newTodo,
+    completed: false,
+  });
+  console.log(todoList);
+
+  // push to local storage
+  localStorage.setItem("todos", JSON.stringify(todoList));
+
+  // Set input back to empty
+  input.value = "";
+
+  render();
+}
+
+function render() {
+  const prevTodos = localStorage.getItem("todos");
+  console.log(prevTodos);
+  todoList = JSON.parse(prevTodos) || [];
   console.log(todoList);
 }
